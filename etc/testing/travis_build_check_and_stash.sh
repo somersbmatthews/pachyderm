6449b3ac4807b49a0e8ad87d73746ccfe611e12f
Somers B Matthews <somersbmatthews@gmail.com>
2020-11-16 11:47:49 -0600

    pulling repo info from env var

--- etc/testing/travis_build_check_and_stash.sh
+++ etc/testing/travis_build_check_and_stash.sh
@@ -18,7 +18,7 @@ set -ex
 # time out, give up. If we succeed, stash that code in a docker image which
 # later tests can use to get it back.
 
-cd /home/travis/gopath/src/github.com/pachyderm/pachyderm
+cd /home/travis/gopath/src/github.com/${TRAVIS_REPO_SLUG}
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     # These shenannigans not needed for release and branch builds, hopefully.
